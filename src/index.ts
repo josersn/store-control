e2c9f152713e46d316fd8a9c1201d23f4d71ac9f
josersn <nettorammos@hotmail.com>
2023-06-14 13:34:51 -0300

    feat: pattern

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "./infrastructure/database";
 import app from "./infrastructure/server";
 
 async function main() {
@@ -5,11 +6,22 @@ async function main() {
 
   application.then((server) => {
     const port = Number(process.env.APP_PORT) ?? 3000;
-    server.listen({ port, host: "0.0.0.0" }, (err, address) => {
-      if (err) throw err;
-
-      console.log("Running");
-    });
+    server
+      .listen({ port, host: "0.0.0.0" })
+      .then((_) => {
+        process
+          .on("SIGTERM", () => {
+            prismaClient.$disconnect();
+            process.exit(0);
+          })
+          .on("SIGINT", () => {
+            prismaClient.$disconnect();
+            process.exit(0);
+          });
+      })
+      .catch((err) => {
+        process.exit(1);
+      });
   });
 }
 
