b552a18015557fc5d92d9cc415662cff175a9c52
josersn <nettorammos@hotmail.com>
2023-06-14 10:35:45 -0300

    chore: improve use case

--- src/application/use-cases/transaction/create-transaction/create-transaction.use-case.ts
+++ src/application/use-cases/transaction/create-transaction/create-transaction.use-case.ts
@@ -1,5 +1,15 @@
-class CreateTransactionUseCase {
-  exec(data: any) {
+import { IUseCase } from "../../../core/use-case";
+
+interface IRequest {
+  value: number;
+  transactionType: string;
+  paymentMethod: string;
+}
+
+type ICreateTransactionUseCase = IUseCase<IRequest, any>;
+
+class CreateTransactionUseCase implements ICreateTransactionUseCase {
+  async exec(data: IRequest): Promise<any> {
     return {
       ...data,
       id: 1,
