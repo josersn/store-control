678164262f1362c0c5dd82468cfe2b7f450251e0
josersn <nettorammos@hotmail.com>
2023-06-14 10:53:52 -0300

    feat: create transaction entity

--- src/domain/entities/transaction.ts
+++ src/domain/entities/transaction.ts
@@ -1,14 +1,14 @@
 import { PaymentMethod } from "../enums/paymentMethod";
 import { TransactionType } from "../enums/transactionType";
 
-interface TransactionDTO {
-  id?: number;
+class Transaction {
+  id: number;
   value: number;
   transactionType: TransactionType;
   paymentMethod: PaymentMethod;
   isActive: boolean;
-  createdAt?: Date;
-  updatedAt?: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export { TransactionDTO };
+export { Transaction };
