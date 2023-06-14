47725f28ee974e893e93c511ee0c4f3eec453e23
josersn <nettorammos@hotmail.com>
2023-06-14 13:53:39 -0300

    chore: implements logger

--- src/infrastructure/controllers/transaction.controller.ts
+++ src/infrastructure/controllers/transaction.controller.ts
@@ -2,10 +2,17 @@ import { FastifyReply } from "fastify";
 import { Controller, POST } from "fastify-decorators";
 import { TransactionService } from "../../application/services/transaction.service";
 import { CreateTransactionUseCase } from "../../application/use-cases/transaction/create-transaction/create-transaction.use-case";
+import { Logger } from "../adapter/logger";
 import { TransactionRepositoryPrisma } from "../database/prisma/repositories/transaction-repository.prisma";
 
 @Controller("transaction")
 export default class TransactionController {
+  private logger;
+
+  constructor() {
+    this.logger = new Logger("TransactionController");
+  }
+
   @POST("/")
   async create(req, reply: FastifyReply) {
     try {
@@ -15,9 +22,10 @@ export default class TransactionController {
 
       const transaction = await useCase.exec(req.body);
 
+      this.logger.info("Transaction Created with success");
       return reply.status(201).send(transaction);
     } catch (error) {
-      console.log("aeeeeeecç",error)
+      this.logger.error(`Error to Create transaction | ${error}`);
       return reply.status(500).send();
     }
   }
